4cbdc5e3e1d2863e7c0ab7f864bd4a415add774f
BobWong <15959187562@qq.com>
2018-11-23 22:32:17 +0800

    1.增加移除操作

--- container/container_util.go
+++ container/container_util.go
@@ -35,3 +35,17 @@ func ContainsString(sl []string, v string) bool {
 	}
 	return false
 }
+
+func RemoveElement(slice []interface{}, elem interface{}) []interface{}{
+	if len(slice) == 0 {
+		return slice
+	}
+	for i, v := range slice {
+		if v == elem {
+			slice = append(slice[:i], slice[i+1:]...)
+			return remove(slice,elem)
+			break
+		}
+	}
+	return slice
+}
