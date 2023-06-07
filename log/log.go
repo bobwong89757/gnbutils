174b85ae40b058e0a51b0b95ddad58374fd3c256
BobWong <15959187562@qq.com>
2023-06-07 15:57:03 +0800

    feat: log支持传入文件名

--- log/log.go
+++ log/log.go
@@ -1,7 +1,9 @@
 package log
 
 import (
+	"fmt"
 	"github.com/bobwong89757/golog/logs"
+	"strings"
 )
 
 var MLog = &Log{}
@@ -10,10 +12,10 @@ type Log struct {
 	logger *logs.BeeLogger
 }
 
-func (l *Log) InitLog(logConfig map[string]string) {
+func (l *Log) InitLog(logConfig map[string]string, logFileName string) {
 	// l.logger.Reset()
 	l.logger = logs.NewLogger()
-	l.logger.SetLogger(logConfig["type"], logConfig["config"])
+	l.logger.SetLogger(logConfig["type"], strings.Replace(logConfig["config"], "default.log", fmt.Sprintf("%s.log", logFileName), -1))
 }
 
 func (l *Log) GetLog() *logs.BeeLogger {
