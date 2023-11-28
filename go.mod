68c59b340b6e478d5062fc4b2f6b40d5a000d244
BobWong <bingqilin89757@gmail.com>
2023-11-28 09:43:35 +0800

    feat: using zap as log framework

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/bobwong89757/gnbutils
 go 1.18
 
 require (
-	github.com/bobwong89757/golog v0.0.2
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/redis/go-redis/v9 v9.0.2
+	go.uber.org/zap v1.26.0
 	golang.org/x/exp v0.0.0-20230224173230-c95f2b4c22f2
 	golang.org/x/text v0.7.0
 	gopkg.in/yaml.v2 v2.2.8
@@ -18,5 +19,8 @@ require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
+	github.com/jonboulle/clockwork v0.4.0 // indirect
+	github.com/lestrrat-go/strftime v1.0.6 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
 )
--- log/log.go
+++ log/log.go
@@ -1,23 +1,78 @@
 package log
 
 import (
-	"fmt"
-	"github.com/bobwong89757/golog/logs"
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"io"
 	"strings"
+	"time"
 )
 
 var MLog = &Log{}
 
 type Log struct {
-	logger *logs.BeeLogger
+	logger *zap.SugaredLogger
 }
 
 func (l *Log) InitLog(logConfig map[string]string, logFileName string) {
-	// l.logger.Reset()
-	l.logger = logs.NewLogger()
-	l.logger.SetLogger(logConfig["type"], strings.Replace(logConfig["config"], "default.log", fmt.Sprintf("%s.log", logFileName), -1))
+
+	// 设置一些基本日志格式 具体含义还比较好理解，直接看zap源码也不难懂
+	encoder := zapcore.NewConsoleEncoder(zapcore.EncoderConfig{
+		MessageKey:  "msg",
+		LevelKey:    "level",
+		EncodeLevel: zapcore.CapitalLevelEncoder,
+		TimeKey:     "ts",
+		EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+			enc.AppendString(t.Format("2006-01-02 15:04:05"))
+		},
+		CallerKey:    "file",
+		EncodeCaller: zapcore.ShortCallerEncoder,
+		EncodeDuration: func(d time.Duration, enc zapcore.PrimitiveArrayEncoder) {
+			enc.AppendInt64(int64(d) / 1000000)
+		},
+	})
+	// 实现两个判断日志等级的interface
+	infoLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.InfoLevel
+	})
+
+	errorLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.ErrorLevel
+	})
+
+	// 获取 info、error日志文件的io.Writer 抽象 getWriter() 在下方实现
+	infoWriter := getWriter("./logs/info.log")
+	errorWriter := getWriter("./logs/error.log")
+
+	// 最后创建具体的Logger
+	core := zapcore.NewTee(
+		zapcore.NewCore(encoder, zapcore.AddSync(infoWriter), infoLevel),
+		zapcore.NewCore(encoder, zapcore.AddSync(errorWriter), errorLevel),
+	)
+
+	// 需要传入 zap.AddCaller() 才会显示打日志点的文件名和行数
+	log := zap.New(core, zap.AddCaller())
+	l.logger = log.Sugar()
 }
 
-func (l *Log) GetLog() *logs.BeeLogger {
+func (l *Log) GetLog() *zap.SugaredLogger {
 	return l.logger
+}
+
+func getWriter(filename string) io.Writer {
+	// 生成rotatelogs的Logger 实际生成的文件名 demo.log.YYmmddHH
+	// demo.log是指向最新日志的链接
+	// 保存7天内的日志，每1小时(整点)分割一次日志
+	hook, err := rotatelogs.New(
+		strings.Replace(filename, ".log", "", -1)+"-%Y%m%d%H.log", // 没有使用go风格反人类的format格式
+		rotatelogs.WithLinkName(filename),
+		rotatelogs.WithMaxAge(time.Hour*24*7),
+		rotatelogs.WithRotationTime(time.Hour),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+	return hook
 }
