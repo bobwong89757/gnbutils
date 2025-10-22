ee6bb6ce348565b190104ed20ad2fa490abf6b08
bob <bobwong@echobees.com>
2025-10-22 18:01:23 +0800

    feat: 替换日志为 zerolog

--- log/log.go
+++ log/log.go
@@ -2,110 +2,98 @@ package log
 
 import (
 	"fmt"
-	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
+	"github.com/natefinch/lumberjack"
+	"github.com/rs/zerolog"
 	"io"
 	"os"
 	"strings"
-	"time"
 )
 
 var MLog = &Log{}
 
 type Log struct {
-	logger *zap.SugaredLogger
+	logger zerolog.Logger
 }
 
+// InitLog 初始化日志系统
 func (l *Log) InitLog(logConfig map[string]string, logFileName string) {
+	logType := strings.ToLower(logConfig["type"]) // console / file / hybrid
+	useColor := strings.EqualFold(logConfig["color"], "true")
 
-	levelEncoder := zapcore.CapitalLevelEncoder
-	useColor, ok := logConfig["color"]
-	if ok {
-		if strings.EqualFold(useColor, "true") {
-			levelEncoder = zapcore.CapitalColorLevelEncoder
+	// 默认日志等级
+	levelStr := strings.ToLower(logConfig["level"])
+	level, err := zerolog.ParseLevel(levelStr)
+	if err != nil {
+		level = zerolog.InfoLevel
+	}
+	zerolog.SetGlobalLevel(level)
+
+	// ========== 日志输出 Writer 组合 ==========
+	var writers []io.Writer
+
+	// 控制台输出
+	if logType == "console" || logType == "hybrid" {
+		consoleWriter := zerolog.ConsoleWriter{
+			Out:        os.Stdout,
+			TimeFormat: "2006-01-02 15:04:05",
+			NoColor:    !useColor,
 		}
+		writers = append(writers, consoleWriter)
 	}
 
-	logType, _ := logConfig["type"]
-
-	// 设置一些基本日志格式 具体含义还比较好理解，直接看zap源码也不难懂
-	encoder := zapcore.NewConsoleEncoder(zapcore.EncoderConfig{
-		MessageKey:  "msg",
-		LevelKey:    "level",
-		EncodeLevel: levelEncoder,
-		TimeKey:     "ts",
-		EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
-			enc.AppendString(t.Format("2006-01-02 15:04:05"))
-		},
-		CallerKey:    "file",
-		EncodeCaller: zapcore.ShortCallerEncoder,
-		EncodeDuration: func(d time.Duration, enc zapcore.PrimitiveArrayEncoder) {
-			enc.AppendInt64(int64(d) / 1000000)
-		},
-	})
-	// 实现两个判断日志等级的interface
-	debugLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-		return lvl >= zapcore.DebugLevel && lvl < zapcore.ErrorLevel
-	})
-
-	errorLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-		return lvl >= zapcore.ErrorLevel
-	})
-
-	// 获取 info、error日志文件的io.Writer 抽象 getWriter() 在下方实现
-	infoWriter := getWriter(fmt.Sprintf("./logs/%s_info.log", logFileName))
-	errorWriter := getWriter(fmt.Sprintf("./logs/%s_error.log", logFileName))
-
-	// 最后创建具体的Logger
-	core := zapcore.NewTee(
-
-		zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), debugLevel),
-		zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), errorLevel),
-	)
-	if strings.EqualFold(logType, "console") {
-		core = zapcore.NewTee(
-			zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), debugLevel),
-			zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), errorLevel),
-		)
-	}
-	if strings.EqualFold(logType, "file") {
-		core = zapcore.NewTee(
-			zapcore.NewCore(encoder, zapcore.AddSync(infoWriter), debugLevel),
-			zapcore.NewCore(encoder, zapcore.AddSync(errorWriter), errorLevel),
-		)
-	}
-	if strings.EqualFold(logType, "hybrid") {
-		core = zapcore.NewTee(
-			zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), debugLevel),
-			zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), errorLevel),
-			zapcore.NewCore(encoder, zapcore.AddSync(infoWriter), debugLevel),
-			zapcore.NewCore(encoder, zapcore.AddSync(errorWriter), errorLevel),
-		)
+	// 文件输出
+	if logType == "file" || logType == "hybrid" {
+		infoWriter := getWriter(fmt.Sprintf("./logs/%s_info.log", logFileName))
+		errorWriter := getWriter(fmt.Sprintf("./logs/%s_error.log", logFileName))
+		writers = append(writers, multiLevelWriter(infoWriter, errorWriter))
 	}
 
-	// 需要传入 zap.AddCaller() 才会显示打日志点的文件名和行数
-	log := zap.New(core, zap.AddCaller())
-	l.logger = log.Sugar()
+	multi := io.MultiWriter(writers...)
+	logger := zerolog.New(multi).With().Timestamp().Caller().Logger()
+
+	l.logger = logger
 }
 
-func (l *Log) GetLog() *zap.SugaredLogger {
+// GetLog 获取 zerolog.Logger
+func (l *Log) GetLog() zerolog.Logger {
 	return l.logger
 }
 
+// getWriter 日志文件滚动
 func getWriter(filename string) io.Writer {
-	// 生成rotatelogs的Logger 实际生成的文件名 demo.log.YYmmddHH
-	// demo.log是指向最新日志的链接
-	// 保存7天内的日志，每1小时(整点)分割一次日志
-	hook, err := rotatelogs.New(
-		strings.Replace(filename, ".log", "", -1)+"-%Y%m%d%H.log", // 没有使用go风格反人类的format格式
-		rotatelogs.WithLinkName(filename),
-		rotatelogs.WithMaxAge(time.Hour*24*7),
-		rotatelogs.WithRotationTime(time.Hour),
-	)
-
-	if err != nil {
-		panic(err)
+	return &lumberjack.Logger{
+		Filename:   filename,
+		MaxSize:    100,  // 单文件最大100MB
+		MaxBackups: 7,    // 保留7个旧文件
+		MaxAge:     30,   // 保留30天
+		Compress:   true, // 压缩旧日志
 	}
-	return hook
+}
+
+// multiLevelWriter 区分 info / error 级别文件
+func multiLevelWriter(infoWriter, errorWriter io.Writer) io.Writer {
+	return zerolog.MultiLevelWriter(levelWriter{
+		infoWriter:  infoWriter,
+		errorWriter: errorWriter,
+	})
+}
+
+// levelWriter 实现根据日志级别分流到不同文件
+type levelWriter struct {
+	infoWriter  io.Writer
+	errorWriter io.Writer
+}
+
+func (lw levelWriter) Write(p []byte) (n int, err error) {
+	// 默认写入 info 文件
+	return lw.infoWriter.Write(p)
+}
+
+func (lw levelWriter) WriteLevel(level zerolog.Level, p []byte) (n int, err error) {
+	switch level {
+	case zerolog.ErrorLevel, zerolog.FatalLevel, zerolog.PanicLevel:
+		return lw.errorWriter.Write(p)
+	default:
+		return lw.infoWriter.Write(p)
+	}
 }
